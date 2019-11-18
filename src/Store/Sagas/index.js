442633ac5f35d9cfd0e41b394ccb0362f487a3a7
neverwinter-sjh <neverwinter.sjh@gmail.com>
2019-11-18 23:01:00 +0900

    eol 수정

--- src/Store/Sagas/index.js
+++ src/Store/Sagas/index.js
@@ -6,7 +6,10 @@ function* fetchTemp(actionObj) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${actionObj.name}&appid=${apiKey}&units=metric`;
 
   const json = yield fetch(url).then(response => response.json());
-  yield [put({ type: 'TEMP_RECEIVED', json: json.main.temp }), put(push(`/city:${json.name}`))];
+  yield [
+    put({ type: 'TEMP_RECEIVED', json: json.main.temp }),
+    put(push(`/city:${json.name}`))
+  ];
 }
 
 function* actionWatcher() {
